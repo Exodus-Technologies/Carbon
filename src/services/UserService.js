591aaa2f28104a52124eddabbe6249afe93fec54
Dwain Robinson <robinson.dwain.developer@gmail.com>
2022-02-19 22:09:49 -0600

    Adding password exclusion from getQueries and updating messaging in buildspec.yml

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -5,7 +5,24 @@ import { badRequest } from '../codes';
 
 const { User } = models;
 
-const queryOps = { __v: 0, _id: 0 };
+const queryOps = { __v: 0, _id: 0, password: 0 };
+
+exports.getUsers = async query => {
+  try {
+    const users = await User.find(query, queryOps);
+    if (users.length) {
+      return {
+        statusCode: 200,
+        items: users
+      };
+    } else {
+      return badRequest(`No users found with selected query params.`);
+    }
+  } catch (err) {
+    console.log('Error getting all users: ', err);
+    return badImplementationRequest('Error getting users.');
+  }
+};
 
 exports.createUser = async payload => {
   const { email } = payload;
@@ -24,22 +41,5 @@ exports.createUser = async payload => {
   } catch (err) {
     console.log(`Error creating user: `, err);
     return badImplementationRequest('Error creating user.');
-  }
-};
-
-exports.getUsers = async query => {
-  try {
-    const users = await User.find(query, queryOps);
-    if (users.length) {
-      return {
-        statusCode: 200,
-        items: users
-      };
-    } else {
-      return badRequest('No users found with selected query params.');
-    }
-  } catch (err) {
-    console.log('Error getting all users: ', err);
-    return badImplementationRequest('Error getting users.');
   }
 };
