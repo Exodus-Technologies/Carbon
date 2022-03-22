8d195eb0abb3f47221705fd6907f93b5310bc97a
Dwain Robinson <robinson.dwain.developer@gmail.com>
2022-03-21 22:42:19 -0500

    Updating application response values and structure

--- src/routes/appRouter.js
+++ src/routes/appRouter.js
@@ -7,17 +7,13 @@ const { Router } = express;
 const router = Router();
 
 router.get('/auth-service/', (_, res) => {
-  res.json({
-    statusCode: 200,
-    message: 'Welcome to Carbon Auth Manager Service!'
-  });
+  res.status(200).send({ message: 'Welcome to Carbon Auth Manager Service!' });
 });
 
 router.get('/auth-service/probeCheck', (_, res) => {
-  res.json({
-    statusCode: 200,
-    message: 'Carbon Auth Manager service up and running!'
-  });
+  res
+    .status(200)
+    .send({ message: 'Carbon Auth Manager service up and running!' });
 });
 
 export default router;
